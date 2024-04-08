1c9919ac468adab89d4705284f5a82a09e67d3ef
erturkmens <sura.erturkmen@commencis.com>
2024-04-08 14:59:24 +0300

    fix: delete

--- src/main/java/seniorproject/core/dataAccess/UserDao.java
+++
@@ -1,7 +0,0 @@
-package seniorproject.core.dataAccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import seniorproject.core.entities.User;
-
-public interface UserDao extends JpaRepository<User, Long> {
-}
