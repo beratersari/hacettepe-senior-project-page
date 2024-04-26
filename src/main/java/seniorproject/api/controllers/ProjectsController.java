a9ce9e66f813cab5c5986e3a65fba1ee80475fc2
beratersari <beratersari0@gmail.com>
2024-04-26 13:11:38 +0300

    Authantication is fixed

--- src/main/java/seniorproject/api/controllers/ProjectsController.java
+++ src/main/java/seniorproject/api/controllers/ProjectsController.java
@@ -25,8 +25,6 @@ public class ProjectsController {
 
 
     @GetMapping("/getAllByPage")
-    //authorization
-    @PreAuthorize("hasRole('ADMIN')")
     public DataResult<List<ProjectDto>> getAllByPage(@RequestParam(name = "pageNo") int pageNo, @RequestParam("pageSize") int pageSize) {
         return this.projectService.getAll(pageNo, pageSize);
     }
